ea36ae0736adf93f625e871260ef24e11e8d4dbc
paola andrea <murillojimenezpaola1@gmail.com>
2024-08-21 22:11:35 -0500

    ejercicio 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+//Ejercicio 1: Comparar Edad //
+
+let edad = prompt("ingrese su edad")
+let edadNumerica = parseInt(edad);
+if (edad >= 18) {
+  console.log("Eres mayor de edad")
+}
+else {
+  console.log("Eres menor de edad")
+
+}
+
+//----------------------------------------------------------------------------
